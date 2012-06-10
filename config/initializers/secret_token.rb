02240552422364ebcccac4344af29ae2553797de
Joseph Dayo <joseph.dayo@gmail.com>
2012-06-10 06:38:06 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appstash::Application.config.secret_token = '8e0c57edb8f85d0ecd3ccca7efcc3a26995128917b2df766dbe2de2dc44b6077afc174fb59c85e234074f834426788bb633e1d41612b25611ccb6de6d192a64e'
